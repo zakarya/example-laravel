892e5c6ca1bdb7911c2a7724e36e53f7b7ab61c3
Zack Vera <qphys50@gmail.com>
2024-09-14 11:28:05 -0700

    day 2 code

---
+++ resources/views/contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <p>Zack Vera</p>
+    <p>(555)123-4567</p>
+    <p>123 Example Pl</p>
+    <p>Schenectedy, NY 12345</p>
+</body>
+</html>
