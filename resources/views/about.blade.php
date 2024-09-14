81b9f01c28621dcef3be6977400cf25b029db95d
Zack Vera <qphys50@gmail.com>
2024-09-14 14:39:36 -0700

    Day 4 Code

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>Hello from the about page!</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
     <h1>Contact Page</h1>
     <p>Zack Vera</p>
     <p>(555)123-4567</p>
