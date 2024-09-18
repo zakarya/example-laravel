156fd832d16ee410327c05863e01e8fe28cd6755
Zack Vera <qphys50@gmail.com>
2024-09-17 21:19:38 -0700

    Day 10 code: working with factories and models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory;
+    
     protected $table = "job_listings";
     protected $fillable = ['title', 'salary'];
 }
