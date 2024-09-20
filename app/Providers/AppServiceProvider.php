88d167927bfd57483b594244066502c34804fd61
Zack Vera <qphys50@gmail.com>
2024-09-19 21:44:54 -0700

    Day 13 and day 14: the n+1 problem and turning off lazy loading, then pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading();
+
+        // Paginator::useBootstrapFive();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ Route::get('/', function () {
 });
 
 Route::get('jobs', function () {
+    $jobs = Job::with('employer')->paginate(3);
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
