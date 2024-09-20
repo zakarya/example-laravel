184187bea59d2cdcf8943b8c710ab5dfebdc067b
Zack Vera <qphys50@gmail.com>
2024-09-20 16:37:15 -0700

    Day 16 code: Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,33 @@ Route::get('/', function () {
 });
 
 Route::get('jobs', function () {
-    $jobs = Job::with('employer')->paginate(3);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->paginate(3);
+
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('jobs', function () {
+    // validation...
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('jobs');
 });
 
 Route::get('/contact', function () {
