22b1ed413431c43c2b749e6ac501f56bec492fd6
Zack Vera <qphys50@gmail.com>
2024-09-21 20:18:40 -0700

    Day 17 code: Added create job button and studied validation

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,11 @@ Route::get('/jobs/{id}', function ($id) {
 });
 
 Route::post('jobs', function () {
-    // validation...
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
