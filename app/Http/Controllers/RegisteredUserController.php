217abe7cf97e8cd412f4f6504c86e73d569cd560
Zack Vera <qphys50@gmail.com>
2024-09-24 15:19:52 -0700

    Day 22 code: make a login and register system from scratch

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,19 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'], //password_confirmation
+        ]);
+
+        //Create the User
+        $user = User::create($attributes);
+        //Log in
+        Auth::login($user);
+        //redirect somewhere
+        return redirect('/jobs');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,6 +42,14 @@
                                 <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
                             @endguest
 
+                            @auth
+                                <form method="POST" action="/logout">
+                                    @csrf
+
+                                    <x-form-button>Log Out</x-form-button>
+                                </form>
+                            @endauth
+
                         </div>
                     </div>
                     <div class="-mr-2 flex md:hidden">
